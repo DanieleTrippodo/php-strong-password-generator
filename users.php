50ac04ad0a964a02d019cc5bd3140d73414b5619
DanieleTrippodo <trippododaniele@gmail.com>
2024-06-28 15:19:22 +0200

    Scaffolding di base

---
+++ users.php
@@ -0,0 +1,17 @@
+<?php
+// ? Tiene conto delle sessioni e dati dell'utente
+$users = [
+    [
+        'username' => 'user1',
+        'password' => 'password1'
+    ],
+    [
+        'username' => 'user2',
+        'password' => 'password2'
+    ],
+    [
+        'username' => 'user3',
+        'password' => 'password3'
+    ]
+];
+?>
